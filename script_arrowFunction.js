7af7f82f4385bd69f5d3b8530693b0c1f1f951a3
Diksha Verma <162664397+dfirev@users.noreply.github.com>
2025-07-17 21:10:52 +0530

    script_arrowFunction.js

---
+++ script_arrowFunction.js
@@ -0,0 +1,17 @@
+//Arrow Function
+let order_list = [100,400,200,600,100]
+let threshold = 5
+let total_cost = (order_list,threshold)=>{
+    let price = 0
+    for(let i = 0; i < order_list.length; i++) {
+        price += order_list[i]
+    }
+    if(order_list.length >= threshold){
+        discount = price/10
+        price = price - discount
+        console.log(`Total price of the order is ${price} with discount of ${discount}`)
+        return
+    }
+    console.log(`Total price of the order is ${price} with no discount`)    
+}
+total_cost(order_list,threshold)
